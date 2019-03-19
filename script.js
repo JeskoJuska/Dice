5b30e6f722cb6348df8804e84e4fd01fd06ea96d
Ksu <yarks01@yandex.ru>
2019-03-19 19:13:25 +0300

    Final version

--- script.js
+++ script.js
@@ -8,28 +8,26 @@ function random(){
   document.querySelector("img.img1").setAttribute("src", image_1);
   document.querySelector("img.img2").setAttribute("src", image_2);
 
+  var flag;
+  var text;
+
   if (count_1>count_2){
-    document.getElementsByTagName("h1")[0].textContent="Player 1 wins!";
+  document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+
   } else if (count_2>count_1){
 
-    document.getElementsByClassName("container")[0].removeChild(document.getElementsByTagName("h1")[0]);
-
-
-var flag = document.createElement("img");
-flag.classList.add("flag");
-flag.src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/triangular-flag-on-post_1f6a9.png";
-var text = document.createElement("h1");
-text.textContent = "grdydytdytd";
-text.classList.add("dice");
-var header = document.createElement("div");
-header.appendChild(flag);
-header.appendChild(text);
-
-//new botton is going to be here
-
-document.getElementsByClassName("container")[0].insertBefore(header, document.getElementsByTagName("div")[1]);
-
-    // document.getElementsByTagName("h1")[0].textContent="Player 2 wins!";
+document.querySelector("h1").innerHTML="Player 2 wins!🚩";
+// flag = document.createElement("img");
+// flag.classList.add("flag");
+// flag.src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/triangular-flag-on-post_1f6a9.png";
+// text = document.createElement("h1");
+// text.textContent = "Player 2 wins!";
+// text.classList.add("dice");
+// var header = document.createElement("div");
+// header.appendChild(text);
+// header.appendChild(flag);
+//
+// document.getElementsByClassName("container")[0].insertBefore(header, document.getElementsByTagName("div")[1]);
   } else{
     document.getElementsByTagName("h1")[0].textContent="Draw!";
   }
