337f7f683820ba4b0837b8ff0d12ea9fa92399a6
Ksenia Yaremenko <44950983+JeskoJuska@users.noreply.github.com>
2019-09-07 14:30:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,17 +17,6 @@ function random(){
   } else if (count_2>count_1){
 
 document.querySelector("h1").innerHTML="Player 2 wins!🚩";
-// flag = document.createElement("img");
-// flag.classList.add("flag");
-// flag.src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/155/triangular-flag-on-post_1f6a9.png";
-// text = document.createElement("h1");
-// text.textContent = "Player 2 wins!";
-// text.classList.add("dice");
-// var header = document.createElement("div");
-// header.appendChild(text);
-// header.appendChild(flag);
-//
-// document.getElementsByClassName("container")[0].insertBefore(header, document.getElementsByTagName("div")[1]);
   } else{
     document.getElementsByTagName("h1")[0].textContent="Draw!";
   }
